20134e0917b0c5b1768f56b1a55c7a71c7c2ac8a
Lasnine <lasninemiranda@gmail.com>
2026-02-19 10:56:35 -0300

    Aula5

---
+++ Senai/Aula5/src/routes/person.ts
@@ -0,0 +1,43 @@
+import express, { Request, Response, Router } from 'express';
+
+const router: Router = express.Router();
+const people: object[] = [];
+
+//criando uma rota para cada tipo de dado que vou ter
+//req: receber
+//res: responder
+//put: esse E esse
+//patch: esse Ou esse
+router
+    .post('/registrar', (req: Request, res: Response) =>{
+        const { name, lastname } = req.body
+        people.push({name, lastname}),
+        res.status(200).send({message: `Bem vindo(a) ${name} ${lastname}! Cadastro realizado com sucesso.`})
+    })
+    .get('/usuarios', (req: Request, res: Response) => {
+        res.status(200).send({usuario: people})
+    })
+    .get('/usuarios/:id', (req: Request, res: Response) => {
+        const { id } = req.params
+        let convertedId = Number(id)
+        res.status(200).send({response: people[convertedId]})
+    })
+    .get('/filtro', (req: Request, res: Response) => {
+        const { name, lastname } = req.query
+        res.status(200).send({response: `${name} ${lastname}`})
+    })
+    .put('/atualizar:id', (req: Request, res: Response) => {
+        const { id } = req.body
+        const { name, lastname } = req.body
+        res.status(200).send({response: `Atualizando usuario ${id} -> ${name} ${lastname}`})
+    })
+    .patch('/atualizar:id', (req: Request, res: Response) => {
+        const { id } = req.body
+        const { name } = req.body
+        res.status(200).send(`Nome da pessoa com ID  ${id} foi atualizado para: ${name}`)
+    })
+    .delete('/deletar/:id', (req: Request, res: Response) => {
+        const { id } = req.params
+        res.status(200).send(`Pessoa com o id:  ${id} foi deletada`)
+    })
+export default router;
